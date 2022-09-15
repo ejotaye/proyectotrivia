33dd425df86cab956b76f33713df0e50ae252341
ejotaye <60277327+ejotaye@users.noreply.github.com>
2022-09-15 15:42:07 -0400

    Add files via upload

--- trivia.py
+++ trivia.py
@@ -1,3 +1,4 @@
+from asyncore import write
 import csv
 
 import time
@@ -8,9 +9,10 @@ def trivia():
     
     # Lee los archivos
 
-    csvfile = open('pregunta.csv', 'r')
+    csvfile = open('pregunta.csv')
     data = list(csv.DictReader(csvfile))
     csvfile.close()
+    #print(data)
 
     print('A continuación, indique los nombres de los jugadores:')
     # procedo a preguntar cuantos jugadores desean jugar
@@ -30,6 +32,7 @@ def trivia():
     # jugadorlist_1 = () # listado de respuestas jugador 1
     # jugadorlist_2 = () # Listado de respuestas jugador 2
     
+    # Empezamos a iterar las preguntas alojadas en nuestro archivo csv
     while True:
         for i in data:
             print('Pregunta numero {}:'.format(numero_pregunta))
@@ -40,7 +43,73 @@ def trivia():
             print('2)', i['2'])
             print('3)', i['3'])
             print('4)', i['4'])
-            numero_pregunta +=1         
+            
+            numero_pregunta +=1
+
+            # Procedemos a realizar los input para la selección de cada jugador
+            while True:
+                resp_j1 = int(input('{}: Seleccione el número de la respuesta correcta:'.format(jugador_1)))
+
+                if resp_j1 <= 0 or resp_j1 > 4:
+                    print('Por favor selecciona una opción correcta')
+                    resp_j1 = input('{}: Seleccione el número de la respuesta correcta:'.format(jugador_1))
+                else:
+                    pass
+
+                resp_j2 = int(input('{}: Seleccione el número de la respuesta correcta:'.format(jugador_2)))
+
+                if resp_j2 <= 0 or resp_j2 > 4:
+                    print('Por favor selecciona una opción correcta')
+                    resp_j2 = input('{}: Seleccione el número de la respuesta correcta:'.format(jugador_1))
+                else:
+                    break
+            
+            # Nombramos una variable para hacer print en pantalla de la respuesta correcta
+            respuesta = i['respuesta_correcta']
+            print('La respuesta correcta es: {}'.format(respuesta))
+
+            # Realizamos condicionales para sumar puntos a los jugadores
+            while True:
+                    if resp_j1 == respuesta:
+                        puntaje_1 +=50
+                        break
+                    elif resp_j2 == respuesta:
+                        puntaje_2 +=50
+                    else:
+                        break
+
+            # A continuación procedemos a concluir el ciclo de preguntar cerrando el bucle               
+            if numero_pregunta == 3:
+                break
+
+        # Procedemos a reflejar el ganador anidando condicionales.        
+        if puntaje_1 > puntaje_2:
+            print('¡Felicidades! El {} a ganado'.format(jugador_1))
+            print('Lo siento {} ¡La proxima vez tendrás mas suerte!'.format(jugador_2))
+        elif puntaje_2 > puntaje_1:
+            print('¡Felicidades! El {} a ganado'.format(jugador_2))
+            print('Lo siento {} ¡La proxima vez tendrás mas suerte!'.format(jugador_1))
+        else:
+            print('¡WOW! fue un empate, inténtalo nuevamente :D Mucha suerte.')
+
+        # Creamos un archivo csv para alojar los puntajes históricos
+        header =['jugador 1', 'puntaje1', 'jugador 2', 'puntaje2']
+        csvfile = open('historico.csv', 'w')
+        write = csv.DictWriter(csvfile, fieldnames=header)
+
+        puntajes = {'jugador 1': jugador_1, 'puntaje1': puntaje_1, 'jugador 2': jugador_2, 'puntaje2': puntaje_2}
+
+        write.writerow(puntajes)
+
+        csvfile.close()
+        break
+            
+            
+
+    
+
+
+        
 
 
 if __name__ == '__main__':
@@ -54,7 +123,3 @@ while jugar !='1' and jugar !='2':
 if jugar =='1':
     trivia()   
 else: print('Adios.')
-
-                
-
-       
